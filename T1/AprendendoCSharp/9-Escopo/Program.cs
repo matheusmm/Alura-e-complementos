6c6d805169c142af515ed373340351ea897d4433
Matheus <stag021@benner.com.br>
2019-05-21 15:30:01 -0300

    9 CSharp Alura

---
+++ T1/AprendendoCSharp/9-Escopo/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7_Condicionais
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Projeto 7 - Condicionais");
+
+            int idadeJ = 18;
+            bool responsavel = true;
+            string mensagemAdicional;
+
+            if (responsavel == true)
+            {
+                mensagemAdicional = "João está com Responsável";
+;
+            }
+            else
+            {
+                mensagemAdicional = "João não está com Responsável";
+            }
+
+            if (idadeJ >= 18 && responsavel == true)
+            {
+                Console.WriteLine("Pode entrar");
+                Console.WriteLine(mensagemAdicional);
+            }
+            else
+            {
+                Console.WriteLine("Não pode entrar");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
