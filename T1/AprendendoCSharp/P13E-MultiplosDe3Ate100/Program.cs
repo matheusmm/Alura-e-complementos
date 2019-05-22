adbc45295cc1da7ce2ea5ca0d5ea36a774359f2f
Matheus <stag021@benner.com.br>
2019-05-22 16:55:03 -0300

    Laço de repetiçao 2 e exercicio complementar

---
+++ T1/AprendendoCSharp/P13E-MultiplosDe3Ate100/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P13E_MultiplosDe3Ate100
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Imprimindo todos os multiplos de 3 entre 1 e 100");
+            for (int numero = 3; numero < 100; numero += 3)
+            {
+                Console.WriteLine(numero);
+            }
+            Console.ReadLine();
+        }
+    }
+}
