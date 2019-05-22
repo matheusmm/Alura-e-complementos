bdae8fdcd61d557ca9bb6d7a441e4646b68910f7
Matheus <stag021@benner.com.br>
2019-05-22 15:05:32 -0300

    Alteração nome "10" para "P10" e exercicios relativo ao projeto 13

---
+++ T1/AprendendoCSharp/P13B-LacoDeRepetiçãoWhile/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P13B_LacoDeRepetiçãoWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int contador = 0;
+            while (contador <= 10)
+            {
+                Console.WriteLine(contador);
+                contador++;
+            }
+        Console.ReadLine();
+        }
+    }
+}
