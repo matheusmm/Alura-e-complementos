09146c902f409d54925e7f3d5e0fa8cc2cb86284
Matheus <stag021@benner.com.br>
2019-05-22 17:10:09 -0300

    Aula 1 Alura CSharp 2

--- T1/ByteBank/ByteBank01/Program.cs
+++ T1/ByteBank/ByteBank01/Program.cs
@@ -10,6 +10,20 @@ namespace ByteBank01
     {
         static void Main(string[] args)
         {
+            string titular = "Gabriela M.";
+            int numeroAgencia = 863;
+            int numero = 863146;
+            double saldo = 100;
+
+            string titular2 = "Bruno";
+            int numeroAgencia2 = 863;
+            int numero2 = 864;
+            double saldo2 = 50;
+
         }
+    }
+}
+
+}
     }
 }
