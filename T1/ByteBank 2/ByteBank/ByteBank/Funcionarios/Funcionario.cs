c40335d36cf3c166fd0eb7add14c1c11d593dcaa
Matheus <stag021@benner.com.br>
2019-05-27 14:16:53 -0300

    CSharp 3

---
+++ T1/ByteBank 2/ByteBank/ByteBank/Funcionarios/Funcionario.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.Funcionarios
+{
+    class Funcionario
+    {
+        private int _tipo;
+
+        public Funcionario (int tipo)
+        {
+            _tipo = tipo;
+        }
+        public string Nome { get; set; }
+        public string CPF { get; set; }
+        public double Salario { get; set; }
+        public double Bonificacao()
+        {
+            return Salario * 0.10;
+        }
+        public double GetBonificacao()
+        {
+            if (_tipo == 1)
+                return Salario;
+
+            return Salario * 0.10;
+        }
+    }
+}
---
+++ T1/ByteBank 2/ByteBank/ByteBank/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ByteBank.Funcionarios;
+
+namespace ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario Carlos = new Funcionario(1);
+            Carlos.Nome = "Carlos";
+            Carlos.CPF = "546.585.585-02";
+            Carlos.Salario = 2000;
+
+            Console.WriteLine(Carlos.Nome);
+            Console.WriteLine(Carlos.GetBonificacao());
+
+            Console.ReadLine();
+        }
+    }
+}
