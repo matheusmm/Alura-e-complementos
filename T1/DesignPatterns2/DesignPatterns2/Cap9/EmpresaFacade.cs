b3fe87802cdd1d02ed213030226f824b350f5ef6
Matheus Medeiros <stag021@benner.com.br>
2019-06-19 16:51:49 -0300

    Final Design Patterns

---
+++ T1/DesignPatterns2/DesignPatterns2/Cap9/EmpresaFacade.cs
@@ -0,0 +1,37 @@
+﻿using DesignPatterns2.Cap8;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns2.Cap9
+{
+    class EmpresaFacade
+    {
+        public Cliente BuscaCliente(String cpf)
+        {
+            return new ClienteDAO().BuscaPorCpf(cpf);
+        }
+
+        public Fatura CriaFatura(Cliente cliente, double valor)
+        {
+            Fatura fatura = new Fatura(cliente, valor);
+            return fatura;
+        }
+
+        public Cobranca GeraCobranca(Fatura fatura)
+        {
+            Cobranca cobranca = new Cobranca(Tipo.Boleto, fatura);
+            cobranca.Emite();
+            return cobranca;
+        }
+
+        public ContatoCliente FazContato(Cliente cliente, Cobranca cobranca)
+        {
+            ContatoCliente contato = new ContatoCliente(cliente, cobranca);
+            contato.Dispara();
+            return contato;
+        }
+    }
+}
--- T1/DesignPatterns2/DesignPatterns2/Program.cs
+++ T1/DesignPatterns2/DesignPatterns2/Program.cs
@@ -1,4 +1,5 @@
 ﻿using DesignPatterns2.Cap7;
+using DesignPatterns2.Cap8;
 using System;
 using System.Collections.Generic;
 using System.Data;
@@ -13,18 +14,8 @@ namespace DesignPatterns2
     {
         static void Main(string[] args)
         {
-            FilaDeTrabalho fila = new FilaDeTrabalho();
-            Pedido pedido1 = new Pedido("Please help", 100.00);
-            Pedido pedido2 = new Pedido("I'm stuck inside this CMD", 200.00);
-
-            fila.Adiciona(new PagaPedido(pedido1));
-            fila.Adiciona(new PagaPedido(pedido2));
-
-            fila.Adiciona(new FinalizaPedido(pedido1));
-
-            fila.Processa();
-
-            Console.ReadKey();
+            
+            Console.ReadLine();
         }
     }
 }
